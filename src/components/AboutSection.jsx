4830cadc89aacfb26e325707fb1166b92ad92884
samzik234 <simonazike155@gmail.com>
2025-09-10 18:30:26 +0100

    AboutMe Image Update

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -101,7 +101,7 @@ export default function AboutSection() {
             }`}
           >
             <img
-              src="/assets/img/bg__23.png"
+              src="/assets/img/bg_24.png"
               alt="Mercelina working on design"
               className="rounded-2xl shadow-xl"
             />
